adf28d6bd8ba7280a8128640dfcfa4ebcdbc812d
dakata1337 <poshtazaprezentaciaa@gmail.com>
2022-03-11 13:24:22 +0200

    SMusic.Data: Added ToString() to Artist model

--- SMusic.Data/Models/Artist.cs
+++ SMusic.Data/Models/Artist.cs
@@ -5,4 +5,8 @@ public class Artist
     public int ArtistId { get; private set; }
     public string Name { get; set; } = default!;
     public List<Album> Albums { get; set; } = default!;
+    public override string ToString()
+    {
+        return this.Name;
+    }
 }
