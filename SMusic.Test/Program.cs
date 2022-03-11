674c39793cc3f4f0c9fdfd3a7368c6e5112ed385
dakata1337 <poshtazaprezentaciaa@gmail.com>
2022-03-11 12:44:41 +0200

    SMusic.Test: created test project

---
+++ SMusic.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SMusic.Test;
+using SMusic.Data;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MusicContext context = new MusicContext();
+        using (context)
+        {
+            foreach (var item in context.Artists) {
+                Console.WriteLine($"{item.Name}");
+                foreach (var album in item.Albums) {
+                    Console.WriteLine($" - {album.Name}");
+                }
+            }
+        }
+    }
+}
