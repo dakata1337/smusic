2663c507270959cdf85f942dbe723d18fc79b157
dakata1337 <poshtazaprezentaciaa@gmail.com>
2022-03-17 17:23:58 +0200

    SMusic.Data: added more songs/albums

--- SMusic.Data/Models/Song.cs
+++ SMusic.Data/Models/Song.cs
@@ -2,7 +2,12 @@ namespace SMusic.Data.Models;
 
 public class Song
 {
-    public int SongId { get; private set; }
+    public Song(string title, string length)
+    {
+        this.Title = title;
+        this.Length = length;
+    }
+    public int SongId { get; set; }
     public string Title { get; set; }
     public string Length { get; set; }
     public List<Artist> Artists { get; set; }
--- SMusic.Data/MusicContext.cs
+++ SMusic.Data/MusicContext.cs
@@ -15,22 +15,46 @@ public class MusicContext : DbContext
             Database.EnsureDeleted();
             Database.EnsureCreated();
 
-            var a1 = CreateArtist("Kanye West");
-            var a2 = CreateArtist("Kanye East");
+            // TODO: maybe load albums from external source?
+            var kanyeWest = CreateArtist("Kanye West");
 
-            CreateAlbum("Meth", Album.Genre.HipHop, new List<Artist> { a1, a2 }, new List<Song> {
-                new() {
-                    Title = "Cooking Meth",
-                    Length = "3.41"
-                },
-                new() {
-                    Title = "Smoking Meth",
-                    Length = "3.11"
-                },
-                new() {
-                    Title = "Being Interrogated By The Police",
-                    Length = "9.10"
-                },
+            CreateAlbum("Graduation", Genre.HipHop, new List<Artist> { kanyeWest }, new List<Song> {
+                new("Good Morning", "3:15"),
+                new("Champion", "2:47"),
+                new("Stronger", "5:11"),
+                new("I Wonder", "4:03"),
+                new("Good Life", "3:27"),
+                new("Can't Tell Me Nothing", "4:31"),
+                new("Barry Bonds", "3:24"),
+                new("Drunk and Hot Girls", "5:13"),
+                new("Flashing Lights", "3:57"),
+                new("Everythin I Am", "3:47"),
+                new("The Glory", "3:32"),
+                new("Homecoming", "3:23"),
+                new("Big Brother", "4:47"),
+                new("Good Night", "3:05"),
+            });
+
+            CreateAlbum("The Life Of Pablo", Genre.HipHop, new List<Artist> { kanyeWest }, new List<Song> {
+                new("Ultralight Beam", "5:20"),
+                new("Father Stretch My Hands Pt.1", "2:15"),
+                new("Father Stretch My Hands Pt.2", "2:10"),
+                new("Famous", "3:16"),
+                new("Feedback", "2:27"),
+                new("Low Lights", "2:21"),
+                new("Highlights", "3:19"),
+                new("Freestyle 4", "2:03"),
+                new("I Love Kanye", "0:44"),
+                new("Waves", "3:01"),
+                new("FML", "3:56"),
+                new("Real Friends", "4:11"),
+                new("Wolves", "5:01"),
+                new("Frank's Track", "0:38"),
+                new("30 Hours", "5:23"),
+                new("No More Parties In LA", "6:14"),
+                new("Facts (Charlie Heat Version)", "3:20"),
+                new("Fade", "3:13"),
+                new("Saint Pablo", "6:12"),
             });
 
             this.SaveChanges();
@@ -45,7 +69,7 @@ public class MusicContext : DbContext
         return artist;
     }
 
-    private Album CreateAlbum(string albumName, Album.Genre genre, List<Artist> artists, List<Song> songs)
+    private Album CreateAlbum(string albumName, Genre genre, List<Artist> artists, List<Song> songs)
     {
         Album album = new();
         album.Name = albumName;
