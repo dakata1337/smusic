18b661625695b0c80b5c4037735e732c394c6932
dakata1337 <poshtazaprezentaciaa@gmail.com>
2022-03-20 13:50:08 +0200

    SMusic.Web: implemented full view of albums

--- SMusic.Web/Controllers/HomeController.cs
+++ SMusic.Web/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Albums(int albumId)
+    {
+        ViewBag.AlbumId = albumId;
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
