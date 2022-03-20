8ec8204d9a8c8899f550816cca89bcb0eb6510bc
dakata1337 <poshtazaprezentaciaa@gmail.com>
2022-03-20 13:56:35 +0200

    SMusic.Web: removed nullable context warnings

--- SMusic.Web/Models/ErrorViewModel.cs
+++ SMusic.Web/Models/ErrorViewModel.cs
@@ -2,7 +2,9 @@ namespace SMusic.Web.Models;
 
 public class ErrorViewModel
 {
+    #nullable enable
     public string? RequestId { get; set; }
+    #nullable restore
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
