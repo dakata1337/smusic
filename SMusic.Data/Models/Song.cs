bed93db9c1c8e0dee3a89065858fabcb6da21e49
dakata1337 <poshtazaprezentaciaa@gmail.com>
2022-03-21 10:59:08 +0200

    Web/Data: changed Song length to TimeSpan

--- SMusic.Data/Models/Song.cs
+++ SMusic.Data/Models/Song.cs
@@ -2,13 +2,17 @@ namespace SMusic.Data.Models;
 
 public class Song
 {
-    public Song(string title, string length)
+    public Song(string title, TimeSpan length)
     {
         this.Title = title;
         this.Length = length;
     }
     public int SongId { get; set; }
     public string Title { get; set; }
-    public string Length { get; set; }
+    public TimeSpan Length { get; set; }
     public List<Artist> Artists { get; set; }
+
+    public string GetLength()
+        => Length.ToString(@"hh\:mm\:ss")
+                .TrimStart(' ', '0', ':');
 }
