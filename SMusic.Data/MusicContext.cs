07a26188e23335e2877c693fce59cce519a8d8c6
dakata1337 <poshtazaprezentaciaa@gmail.com>
2023-01-20 11:43:10 +0200

    Generate dummy data on init start

--- SMusic.Data/MusicContext.cs
+++ SMusic.Data/MusicContext.cs
@@ -8,12 +8,31 @@ public class MusicContext : DbContext
     public DbSet<Album> Albums { get; set; }
     public DbSet<Artist> Artists { get; set; }
 
-    private static bool _created = false;
     public MusicContext()
     {
-        if (!_created) {
-            _created = true;
-            Database.EnsureCreated();
+        if (Database.EnsureCreated())
+        {
+            Artist a1 = new Artist();
+            a1.Name = "Bruh";
+            Artists.Add(a1);
+
+            Artist a2 = new Artist();
+            a2.Name = "Lil Lol";
+            Artists.Add(a2);
+
+            Album p1 = new Album();
+            p1.Artists = new List<Artist>() {
+                a1, a2
+            };
+            p1.Name = "The big cringe";
+            p1.AlbumGenre = Genre.HipHop;
+            p1.MonthlyListeners = 69420;
+            p1.Songs = new List<Song>() {
+                new Song("N-words in Paris", TimeSpan.FromSeconds(230)),
+                new Song("Le'cringe", TimeSpan.FromSeconds(193)),
+            };
+            Albums.Add(p1);
+
             this.SaveChanges();
         }
     }
